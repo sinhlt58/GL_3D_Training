16903cc831f36405179ab498af7a4f276a7433ff
Sinh Black <sinhlt58@gmail.com>
2017-11-18 19:31:25 +0700

    add dynamic libs and change assimp to release

--- Source/GraphicsEngine/Texture.h
+++ Source/GraphicsEngine/Texture.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <GLES2/gl2.h>
+#include <string>
 
 class Texture
 {
@@ -9,8 +10,11 @@ public:
 	~Texture();
 	GLuint GetTextureID() const;
 	void LoadFile(const char* filePath);
+	void SetType(std::string s) { m_Type = s; }
+	std::string GetType() const { return m_Type; }
 private:
 	GLuint m_ID;
 	GLuint m_TextureID;
+	std::string m_Type;
 	static void SetTextureParamerter();
 };
